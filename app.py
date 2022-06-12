2806d12f75ceb216c6d52cde18df8bc7cb94732a
Keen <bmodi700@gmail.com>
2022-06-12 11:16:45 +0530

    a

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
-def index():
-    return "Starting Machine Learning Project"
+@app.route('/', methods=['GET', 'POST']) # To render Homepage
+def home_page():
+    return "Starting Machine Learning"
 
-if __name__=="main":
+if __name__ == '__main__':
     app.run(debug=True)
